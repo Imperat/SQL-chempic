64637208a1e3f21f79b83c9864ce32e858a1b0ce
Michail Lelyakin <power_2007_pro@mail.ru>
2016-05-09 13:38:12 +0300

    Create data.sql

---
+++ data.sql
@@ -0,0 +1,4 @@
+insert into Positions (id, name) values (1, "GK");
+insert into Positions (id, name) values (2, "FW");
+insert into Positions (id, name) values (3, "DC");
+insert into Positions (id, name) values (4, "MC");
