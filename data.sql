890b63cff499b29f82a6a78d70a3aa94e7fd69ff
Michail Lelyakin <power_2007_pro@mail.ru>
2016-05-09 18:02:11 +0300

    Update data.sql

--- data.sql
+++ data.sql
@@ -2,3 +2,11 @@ insert into Positions (id, name) values (1, "GK");
 insert into Positions (id, name) values (2, "FW");
 insert into Positions (id, name) values (3, "DC");
 insert into Positions (id, name) values (4, "MC");
+
+
+insert into League (id, name, start_date, end_date) values 
+	(1, 'Russian Chempic', '22.04.2006', '22.04.2007');
+insert into League (id, name, start_date, end_date) values 
+	(2, 'Russian Chempic', '25.04.2007', '26.04.2008');
+insert into League (id, name, start_date, end_date) values 
+	(3, 'Kefal', '01.05.2010', '01.05.2011')
