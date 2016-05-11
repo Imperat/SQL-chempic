244c47405d871324e33a4d473827b4439204909e
Michail Lelyakin <power_2007_pro@mail.ru>
2016-05-11 10:22:42 +0300

    Update createDB.sql

--- createDB.sql
+++ createDB.sql
@@ -1,9 +1,10 @@
 CREATE TABLE [Players] (
 	id int NOT NULL,
-	First_Name varchar(255) NOT NULL,
-	Last_Name varchar(255) NOT NULL,
+	First_Name varchar NOT NULL,
+	Last_Name varchar NOT NULL,
 	Birth_Day date NOT NULL,
 	Position int NOT NULL,
+	salary money NOT NULL,
   CONSTRAINT [PK_PLAYERS] PRIMARY KEY CLUSTERED
   (
   [id] ASC
@@ -13,7 +14,7 @@ CREATE TABLE [Players] (
 GO
 CREATE TABLE [Positions] (
 	id int NOT NULL,
-	name varchar(255) NOT NULL,
+	name varchar NOT NULL,
   CONSTRAINT [PK_POSITIONS] PRIMARY KEY CLUSTERED
   (
   [id] ASC
@@ -23,8 +24,10 @@ CREATE TABLE [Positions] (
 GO
 CREATE TABLE [Teams] (
 	id int NOT NULL,
-	name varchar(255) NOT NULL,
-	city varchar(255) NOT NULL,
+	name varchar NOT NULL,
+	city int NOT NULL,
+	making date NOT NULL,
+	budget money NOT NULL,
   CONSTRAINT [PK_TEAMS] PRIMARY KEY CLUSTERED
   (
   [id] ASC
@@ -34,7 +37,7 @@ CREATE TABLE [Teams] (
 GO
 CREATE TABLE [League] (
 	id int NOT NULL,
-	name varchar(255) NOT NULL,
+	name varchar NOT NULL,
 	start_date datetime NOT NULL,
 	end_date datetime NOT NULL,
   CONSTRAINT [PK_LEAGUE] PRIMARY KEY CLUSTERED
@@ -61,7 +64,43 @@ CREATE TABLE [Matches] (
 	away int NOT NULL,
 	home_goals int NOT NULL,
 	away_goals int NOT NULL,
-	league_id int NOT NULL
+	league_id int NOT NULL,
+	stadion int NOT NULL,
+	date datetime NOT NULL
+)
+GO
+CREATE TABLE [Stadions] (
+	id int NOT NULL,
+	name varchar NOT NULL,
+	city int NOT NULL,
+  CONSTRAINT [PK_STADIONS] PRIMARY KEY CLUSTERED
+  (
+  [id] ASC
+  ) WITH (IGNORE_DUP_KEY = OFF)
+
+)
+GO
+CREATE TABLE [Cityes] (
+	id int NOT NULL,
+	name varchar NOT NULL,
+  CONSTRAINT [PK_CITYES] PRIMARY KEY CLUSTERED
+  (
+  [id] ASC
+  ) WITH (IGNORE_DUP_KEY = OFF)
+
+)
+GO
+CREATE TABLE [Goal] (
+	id int NOT NULL,
+	author int NOT NULL,
+	match int NOT NULL,
+	is_penalty binary NOT NULL DEFAULT '0',
+	time time NOT NULL DEFAULT '0',
+  CONSTRAINT [PK_GOAL] PRIMARY KEY CLUSTERED
+  (
+  [id] ASC
+  ) WITH (IGNORE_DUP_KEY = OFF)
+
 )
 GO
 ALTER TABLE [Players] WITH CHECK ADD CONSTRAINT [Players_fk0] FOREIGN KEY ([Position]) REFERENCES [Positions]([id])
@@ -71,6 +110,11 @@ ALTER TABLE [Players] CHECK CONSTRAINT [Players_fk0]
 GO
 
 
+ALTER TABLE [Teams] WITH CHECK ADD CONSTRAINT [Teams_fk0] FOREIGN KEY ([city]) REFERENCES [Cityes]([id])
+ON UPDATE CASCADE
+GO
+ALTER TABLE [Teams] CHECK CONSTRAINT [Teams_fk0]
+GO
 
 
 ALTER TABLE [Players in Teams] WITH CHECK ADD CONSTRAINT [Players in Teams_fk0] FOREIGN KEY ([player_id]) REFERENCES [Players]([id])
@@ -109,4 +153,27 @@ ALTER TABLE [Matches] WITH CHECK ADD CONSTRAINT [Matches_fk2] FOREIGN KEY ([leag
 ON UPDATE CASCADE
 GO
 ALTER TABLE [Matches] CHECK CONSTRAINT [Matches_fk2]
+GO
+ALTER TABLE [Matches] WITH CHECK ADD CONSTRAINT [Matches_fk3] FOREIGN KEY ([stadion]) REFERENCES [Stadions]([id])
+ON UPDATE CASCADE
+GO
+ALTER TABLE [Matches] CHECK CONSTRAINT [Matches_fk3]
+GO
+
+ALTER TABLE [Stadions] WITH CHECK ADD CONSTRAINT [Stadions_fk0] FOREIGN KEY ([city]) REFERENCES [Cityes]([id])
+ON UPDATE CASCADE
+GO
+ALTER TABLE [Stadions] CHECK CONSTRAINT [Stadions_fk0]
+GO
+
+
+ALTER TABLE [Goal] WITH CHECK ADD CONSTRAINT [Goal_fk0] FOREIGN KEY ([author]) REFERENCES [Players]([id])
+ON UPDATE CASCADE
+GO
+ALTER TABLE [Goal] CHECK CONSTRAINT [Goal_fk0]
+GO
+ALTER TABLE [Goal] WITH CHECK ADD CONSTRAINT [Goal_fk1] FOREIGN KEY ([match]) REFERENCES [Teams]([id])
+ON UPDATE CASCADE
+GO
+ALTER TABLE [Goal] CHECK CONSTRAINT [Goal_fk1]
 GO
