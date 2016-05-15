db64170e577459ef38592511c8f94f31347ba3a4
Michail Lelyakin <power_2007_pro@mail.ru>
2016-05-15 22:48:37 +0300

    Update data.sql

--- data.sql
+++ data.sql
@@ -10,3 +10,50 @@ insert into League (id, name, start_date, end_date) values
 	(2, 'Russian Chempic', '25.04.2007', '26.04.2008');
 insert into League (id, name, start_date, end_date) values 
 	(3, 'Kefal', '01.05.2010', '01.05.2011')
+
+insert into Cityes values (1, 'Moscow');
+insert into Cityes values (2, 'Piterga');
+insert into Cityes values (3, 'London');
+insert into Cityes values (4, 'Keyptaun');
+insert into Cityes values (5, 'Dekabrist');
+insert into Cityes values (6, 'New Yourk');
+insert into Cityes values (7, 'Paris');
+insert into Cityes values (8, 'Murmansk');
+insert into Cityes values (9, 'Tula');
+insert into Cityes values (10, 'Ershow');
+insert into Cityes values (11, 'Berlin');
+insert into Cityes values (12, 'Balakovo');
+insert into Cityes values (13, 'Zlatoust');
+insert into Cityes values (14, 'Yakutsk');
+insert into Cityes values (15, 'Perm');
+insert into Cityes values (16, 'Penza');
+insert into Cityes values (17, 'Rtishevo');
+insert into Cityes values (18, 'Tambov');
+insert into Cityes values (19, 'Volgogradez');
+insert into Cityes values (20, 'Saratov');	
+
+insert into Teams values (1, 'Spartak', 1, '2012.04.22', 30.0);
+insert into Teams values (2, 'CSKA', 1, '2012.04.22', 30.0);
+insert into Teams values (3, 'Dinamo', 1, '2012.04.22', 30.0);
+insert into Teams values (4, 'Locomotive', 1, '2012.04.22', 30.0);
+
+insert into Teams values (5, 'Zenit', 2, '2012.04.22', 30.0);
+insert into Teams values (6, 'Arsenal', 3, '2012.04.22', 30.0);
+insert into Teams values (7, 'Chelsea', 3, '2012.04.22', 30.0);
+insert into Teams values (8, 'Club', 4, '2012.04.22', 30.0);
+insert into Teams values (9, 'FC Krishka', 5, '2012.04.22', 30.0);
+insert into Teams values (10, 'New Youk', 6, '2012.04.22', 30.0);
+insert into Teams values (11, 'PSJ', 7, '2012.04.22', 30.0);
+insert into Teams values (12, 'Murmansk', 8, '2012.04.22', 30.0);
+insert into Teams values (13, 'Arsenal', 9, '2012.04.22', 30.0);
+insert into Teams values (14, 'Ershov', 10, '2012.04.22', 30.0);
+insert into Teams values (15, 'Bavaria', 11, '2012.04.22', 30.0);
+insert into Teams values (16, 'Gubernator', 12, '2012.04.22', 30.0);
+insert into Teams values (17, 'Tramvay', 13, '2012.04.22', 30.0);
+insert into Teams values (18, 'Yakutsk', 14, '2012.04.22', 30.0);
+insert into Teams values (19, 'Amkar', 15, '2012.04.22', 30.0);
+insert into Teams values (20, 'Zenit', 16, '2012.04.22', 30.0);
+insert into Teams values (21, 'RJD', 17, '2012.04.22', 30.0);
+insert into Teams values (22, 'Spartak', 18, '2012.04.22', 30.0);
+insert into Teams values (23, 'Rotor', 19, '2012.04.22', 30.0);
+insert into Teams values (24, 'Sokol', 20, '2012.04.22', 30.0);
