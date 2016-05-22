66779389eabab8d9cee67b88f7de6631c3098eac
Michail Lelyakin <power_2007_pro@mail.ru>
2016-05-22 18:35:57 +0300

    Update triggers.sql

--- triggers.sql
+++ triggers.sql
@@ -22,3 +22,28 @@ AS
    PRINT 'Error. Cann"t add goal to non-active Match :( '
    ROLLBACK TRANSACTION
   END
+
+/* Also: */
+
+CREATE TRIGGER golic2 ON Goal
+FOR UPDATE 
+AS
+ DECLARE @author int
+ DECLARE @match int
+ DECLARE @isPenka binary
+ DECLARE @time time
+ DECLARE @isActive binary
+
+ SELECT @author=author,
+		@match=match,
+		@isPenka=is_penalty,
+		@time=time
+ FROM inserted
+
+ SELECT @isActive=isActive from Matches where id=@match
+
+ IF @isActive=0
+  BEGIN 
+   PRINT 'Error. Cann"t change goal to non-active Match :( '
+   ROLLBACK TRANSACTION
+  END
