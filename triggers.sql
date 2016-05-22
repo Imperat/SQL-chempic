4e7ec6ab5d26710a2bc89f53ffaa135260122a54
Michail Lelyakin <power_2007_pro@mail.ru>
2016-05-22 18:31:14 +0300

    Create triggers.sql

---
+++ triggers.sql
@@ -0,0 +1,24 @@
+/* We need add goals only to Mathes with "isActive" state */
+
+CREATE TRIGGER golic ON Goal
+FOR INSERT 
+AS
+ DECLARE @author int
+ DECLARE @match int
+ DECLARE @isPenka binary
+ DECLARE @time time
+ DECLARE @isActive binary
+
+ SELECT @author=author,
+		@match=match,
+		@isPenka=is_penalty,
+		@time=time
+ FROM inserted
+
+ SELECT @isActive=isActive from Matches where id=@match
+
+ IF @isActive=0
+  BEGIN 
+   PRINT 'Error. Cann"t add goal to non-active Match :( '
+   ROLLBACK TRANSACTION
+  END
